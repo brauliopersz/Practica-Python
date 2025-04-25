b9a747adf63355c93f1265d4c1d157cf0cfef51e
brauliopersz <brauliopersz@gmail.com>
2025-04-25 16:41:50 -0400

    Se importan los modulos al main

---
+++ main_conversor.py
@@ -0,0 +1,12 @@
+from conversor_longitud import kilometros, metros, millas, pies
+from conversor_temperatura import celsius, fahrenheit, kelvin
+
+print("""
+      
+      Seleccione el conversor que desea utilizar
+      
+      1. Conversor Longitud
+      2. Conversor Temperatura
+      
+      """)
+
