2e416bd4c9a415de98031d84672cb9df6f835dfe
Braulio <brauliopersz@gmail.com>
2025-04-25 23:03:48 -0400

    Se completa módulo de conversión fahrenheit_to_celsius y fahrenheit_to_kelvin

---
+++ conversor_temperatura/fahrenheit.py
@@ -0,0 +1,9 @@
+def fahrenheit_to_celsius(temperatura):
+    resultado = 0
+    resultado = (temperatura - 32) / 1.8
+    return resultado
+
+def fahrenheit_to_kelvin(temperatura):
+    resultado = 0
+    resultado = (temperatura + 459.67) / 1.8
+    return resultado
