56033b6ef103f21a755d2c2b6bba804447352a82
Braulio <brauliopersz@gmail.com>
2025-04-25 23:02:50 -0400

    Se completa módulo de conversión celcius_to_farenheit y celcius_to_kelvin

---
+++ conversor_temperatura/celsius.py
@@ -0,0 +1,9 @@
+def celcius_to_farenheit(temperatura):
+    resultado = 0
+    resultado = (temperatura * 1.8) + 32
+    return resultado
+
+def celcius_to_kelvin(temperatura):
+    resultado = 0
+    resultado = temperatura + 273.15
+    return resultado
