58241b2e05b5cfdf018375754a9d09e2df79e466
Braulio <brauliopersz@gmail.com>
2025-04-25 23:01:57 -0400

    Se completa módulo de conversión kelvin_to_celsius y kelvin_to_fahrenheit

---
+++ conversor_temperatura/kelvin.py
@@ -0,0 +1,8 @@
+def kelvin_to_celsius(temperatura):
+    resultado = 0
+    resultado = temperatura - 273.15
+
+def kelvin_to_fahrenheit(temperatura):
+    resultado = 0
+    resultado = (temperatura - 273.15) * 9/5 + 32
+    return resultado
